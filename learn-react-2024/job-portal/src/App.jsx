4c98d093eb17090d2163692d7c12cf3819683671
shubham <design@artificialreality.com>
2024-06-03 12:18:10 +0530

    initial

---
+++ learn-react-2024/job-portal/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css';
+
+const App = () => {
+  return (
+    <>
+      <h2 className='text-xl font-bold'>New App</h2>
+    </>
+  )
+}
+
+export default App
