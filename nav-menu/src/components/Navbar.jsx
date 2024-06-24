1c46b7a07a672086e86ed4214bc1baeb9ab29a57
shubham <design@artificialreality.com>
2024-06-24 20:07:55 +0530

    navbar created, conditional classes need to be added

--- nav-menu/src/components/Navbar.jsx
+++ nav-menu/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+
 import { NavLink } from 'react-router-dom'
+import HomeIcon from './icons/HomeIcon'
+import MessageIcon from './icons/MessageIcon'
+import MusicIcon from './icons/MusicIcon'
+import SettingIcons from './icons/SettingIcons'
 const Navbar = () => {
   const linkClass = ({isActive}) => isActive ?"activeLink" : ""
+
+
   return (
     <nav className='navbar-wrapper'>
       <ul>
-        <li className='nav-link'><NavLink to={'/'} className={linkClass} >Home</NavLink></li>
-        <li className='nav-link'><NavLink to={'/message'} className={linkClass} >Messages</NavLink></li>
-        <li className='nav-link'><NavLink to={'/music'} className={linkClass} >Music</NavLink></li>
-        <li className='nav-link'><NavLink to={'/setting'} className={linkClass} >Settings</NavLink></li>
+        <li className='nav-link'><NavLink to={'/'} className={linkClass} ><HomeIcon color= 'red'/></NavLink></li>
+        <li className='nav-link'><NavLink to={'/message'} className={linkClass} ><MessageIcon color= 'blue'/></NavLink></li>
+        <li className='nav-link'><NavLink to={'/music'} className={linkClass} ><MusicIcon color ="orange"/></NavLink></li>
+        <li className='nav-link'><NavLink to={'/setting'} className={linkClass} ><SettingIcons color = "red"/></NavLink></li>
       </ul>
     </nav>
   )
