50b915026fd1dde9c351a718320ff623b919a4e1
shubham <shubhamyelekar0@gmail.com>
2024-07-02 19:41:05 +0530

    toggle switch added

--- portfolio-alpha/src/components/Header.jsx
+++ portfolio-alpha/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Switch from './Switch'
+import Logo from './Logo'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='p-5 pt-2  mx-auto flex items-center item-center justify-between '>
+      <Logo/>
+      <Switch/>
+    </div>
   )
 }
 
---
+++ portfolio-alpha/src/components/Logo.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Logo = () => {
+  return (
+    <Link to={'/'}>
+      <svg width="66" height="32" viewBox="0 0 66 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M51.4125 24.3208L45.5703 30.2331C44.9266 30.8846 45.3816 32 46.2913 32H55.2471C55.5174 32 55.7766 31.8913 55.968 31.6976L61.8101 25.7854C62.4539 25.1339 61.9988 24.0185 61.0892 24.0185H52.1334C51.8631 24.0185 51.6038 24.1272 51.4125 24.3208Z" fill="url(#paint0_linear_51_20)"/>
+      <path d="M56.0554 0C55.7848 0 55.5252 0.109007 55.3339 0.30304L48.4787 7.25332C48.0815 7.65604 47.438 7.65756 47.0389 7.25671L40.1865 0.373548C39.9954 0.181593 39.7373 0.073903 39.4683 0.073903H28.0798H16.6942C16.4235 0.073903 16.1639 0.18291 15.9726 0.376943L0.300936 16.2661C-0.341929 16.9179 0.113377 18.0323 1.02253 18.0323H9.94456C10.2152 18.0323 10.4748 17.9233 10.6662 17.7293L28.0798 0.073903L41.1307 13.2363C41.531 13.64 41.5319 14.2966 41.1326 14.7014L25.8858 30.1599C25.2429 30.8116 25.6982 31.9261 26.6074 31.9261H35.5294C35.8001 31.9261 36.0596 31.8171 36.251 31.6231L65.6991 1.76624C66.3419 1.11446 65.8866 0 64.9775 0H56.0554Z" fill="url(#paint1_linear_51_20)"/>
+      <path d="M6.92818 32C6.01882 32 5.56361 30.8851 6.20689 30.2334L21.6728 14.566C21.8642 14.3722 22.1236 14.2633 22.3941 14.2633H31.3002C32.2096 14.2633 32.6648 15.3782 32.0215 16.0298L16.5556 31.6973C16.3643 31.8911 16.1048 32 15.8343 32H6.92818Z" fill="url(#paint2_linear_51_20)"/>
+      <defs>
+      <linearGradient id="paint0_linear_51_20" x1="33" y1="0" x2="33" y2="32" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#161617"/>
+      <stop offset="1" stopColor="#0D0D0E"/>
+      </linearGradient>
+      <linearGradient id="paint1_linear_51_20" x1="33" y1="0" x2="33" y2="32" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#161617"/>
+      <stop offset="1" stopColor="#0D0D0E"/>
+      </linearGradient>
+      <linearGradient id="paint2_linear_51_20" x1="33" y1="0" x2="33" y2="32" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#161617"/>
+      <stop offset="1" stopColor="#0D0D0E"/>
+      </linearGradient>
+      </defs>
+      </svg>
+    </Link>
+  )
+}
+
+export default Logo
