c25870416cac66c3d9eebacd23670f1dba752d76
shubham <shubhamyelekar0@gmail.com>
2025-05-16 19:15:36 +0530

    removed

--- next-app/nextjs-dashboard/app/ui/fonts.tsx
+++
@@ -1,7 +0,0 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-export const geist = Geist({ subsets: ["latin"] });
-export const geist_mono = Geist_Mono({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
